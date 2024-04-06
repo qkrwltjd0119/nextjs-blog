2aef6c468b12ad8b90c5a2e70ce930b15b13b3cb
박지성 <jiseong@bagjiseong-ui-noteubug.local>
2024-04-06 16:40:41 +0900

    Initial commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>Hello my name is Ji Seong Park, I am currently studying Information Systems in Hanyang university</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
+  
